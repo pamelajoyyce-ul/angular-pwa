eb57c21bb8247d446315c9e6fc7ec1a5c2341a1e
pamelajoyyce <pulacson.halcyondigital@gmail.com>
2020-03-30 13:01:50 +0800

    changed baseURL

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Angular PWA Boilerplate Documentation', // Title for your website.
   tagline: 'A website for documentations of Angular PWA Boilerplate',
   url: 'https://pamelajoyyce-ul.github.io/', // Your website URL
-  baseUrl: '/documentation/', // Base URL for your project */
+  baseUrl: '/angular-pwa/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
